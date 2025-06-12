4f10738eca2faaa693f913981dcb2f0bd2397276
Colin Ding <colinding10@gmail.com>
2025-06-12 00:30:53 -0400

    wow

---
+++ the-ancients/src/helper.py
@@ -0,0 +1,5 @@
+FLAG = "bcactf{my_6r347_6r347_6r347_6r347_6r347_6r347_6r347_6r4ndf47h3r_pr06r4mm3d_7h15}"
+print(f"the flag is {FLAG}")
+print(f"with len {len(FLAG)}")
+
+print(f'ord values: [{", ".join(str(ord(c)) for c in FLAG)}]')

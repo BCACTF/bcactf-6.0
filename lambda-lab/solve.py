86cbf9f1dd5c7bf85dc048da6568d4a25102929d
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2025-06-22 09:09:33 -0400

    update lambda lab to fix cheese and clarify

--- lambda-lab/solve.py
+++ lambda-lab/solve.py
@@ -6,9 +6,6 @@ conn = pwn.remote('localhost', 8080)
 # Open solve.txt and send each line
 with open('solve.txt', 'r') as f:
     for line in f.read().splitlines():
-        if line.strip() == '' or line.startswith(";"):
-            continue
-        # print("sending line: ", line.rstrip('\n'))
         conn.sendline(line.rstrip('\n').encode('utf-8'))
         response = conn.recvline()
         print(response.decode('utf-8', errors='ignore').rstrip())
